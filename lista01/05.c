b6f55f8401b1ad5a67a655fbe4807cd4ed15c85c
Lucas-Molina <32374950+Lucas-Molina@users.noreply.github.com>
2017-09-28 20:13:19 -0300

    adicionado exercicio 05

---
+++ lista01/05.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+   int a, b, c;
+
+   printf("Insira dois numeros para dividir\n");
+   scanf("%d%d",&a,&b);
+
+   c = a/b;
+
+   printf("A divisao dos numeros e = %d\n",c);
+
+   return 0;
+}
