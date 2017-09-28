13b871feac8074e836e6c34a79ef0a1f4191033a
Lucas-Molina <32374950+Lucas-Molina@users.noreply.github.com>
2017-09-28 19:30:56 -0300

    adicionado exercicio 01

---
+++ lista01/01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int maind () {
+
+    int valor, resto;
+    printf("insira um numero: ");
+    scanf("%d", &valor);
+
+    resto=valor%2;
+
+    if (resto == 0) {
+        printf("O numero inserido e par \n");
+    } else {
+        printf("O numero inserido e impar \n");
+    }
+
+    return 0;
+
+ }
