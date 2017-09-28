7ad3f72ac74e7830c1c2e0a0cc1f75629e09db9a
Lucas-Molina <32374950+Lucas-Molina@users.noreply.github.com>
2017-09-28 19:51:16 -0300

    Add files via upload

---
+++ lista01/02.c
@@ -0,0 +1,11 @@
+ O volume na unidade C ‚ OS
+ O N£mero de S‚rie do Volume ‚ C6AB-5584
+
+ Pasta de C:\Users\Lucas\Documents\GitHub\pe_q32017_21073314\pe_q32017_21073314\lista01
+
+28/09/2017  19:44    <DIR>          .
+28/09/2017  19:44    <DIR>          ..
+28/09/2017  19:42               398 01.c
+28/09/2017  19:44                 0 02.c
+               2 arquivo(s)            398 bytes
+               2 pasta(s)   287.184.809.984 bytes dispon¡veis
