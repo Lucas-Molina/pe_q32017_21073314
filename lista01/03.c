9a0647ac52897ebfcdf80919d5e062500754f7bc
Lucas-Molina <32374950+Lucas-Molina@users.noreply.github.com>
2017-09-28 20:05:00 -0300

    adicionado exercicio 03

---
+++ lista01/03.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+   int a, b, c;
+
+   printf("Insira dois numeros para subtrair\n");
+   scanf("%d%d",&a,&b);
+
+   c = a - b;
+
+   printf("A subtracao dos numeros e = %d\n",c);
+
+   return 0;
+}
