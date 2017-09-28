de19a0a8320e13c1140d69488c6db82da1f97a17
Lucas-Molina <32374950+Lucas-Molina@users.noreply.github.com>
2017-09-28 20:01:52 -0300

    adicionado exercicio 2

---
+++ lista01/02.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+int main()
+{
+   int a, b, c;
+ 
+   printf("Insira dois numeros para somar\n");
+   scanf("%d%d",&a,&b);
+ 
+   c = a + b;
+ 
+   printf("A soma dos numeros e = %d\n",c);
+ 
+   return 0;
+}
