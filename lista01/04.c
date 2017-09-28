df1f5a9cf7a136d50dc686b5546683c4bd5e674c
Lucas-Molina <32374950+Lucas-Molina@users.noreply.github.com>
2017-09-28 20:12:55 -0300

    adicionado exercicio 04

---
+++ lista01/04.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+   int a, b, c;
+
+   printf("Insira dois numeros para multiplicar\n");
+   scanf("%d%d",&a,&b);
+
+   c = a * b;
+
+   printf("A multiplicacao dos numeros e = %d\n",c);
+
+   return 0;
+}
